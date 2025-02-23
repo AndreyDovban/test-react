04010ae9b0cb4466d932fa9de0306f52d28aa7c8
AndreyDovban <dovban.andreyy@rambler.ru>
2025-02-23 17:27:23 +0300

    Реалзовано уведомление через поортал

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
+import { useState } from 'react';
 import styles from './App.module.css';
 import { Note } from './components';
 
 export default function App() {
+	const [isOpen, setIsOpen] = useState(false);
+
+	function changeIsOpen(val) {
+		setIsOpen(val);
+	}
+
 	return (
 		<div className={styles.block}>
-			<Note text={'Hello notofication'} />
-			<button>click</button>
+			<Note text={'Hello notofication'} isOpen={isOpen} setIsOpen={changeIsOpen} />
+			<button className={styles.button} onClick={() => changeIsOpen(!isOpen)}>
+				click
+			</button>
 		</div>
 	);
 }
--- src/components/Note/Note.jsx
+++ src/components/Note/Note.jsx
@@ -1,22 +1,30 @@
-import { useEffect } from 'react';
+import { createPortal } from 'react-dom';
 import styles from './Note.module.css';
+import cn from 'classnames';
+import Alarm from '../../assets/svg/alarm.svg?react';
+
+const portal = document.querySelector('#portal');
 
 /**
  * Компонент уведомление
  * @param {string} text Текст уведомления
+ * @param {boolean} isOpen Состояние показать/скрыть блок
+ * @param {function} setIsOpen Изменение состояния показать/скрыть
  * @param {...any} props Неопределённое количество прараметров для работы с HTML элементами
  * @returns {JSXElement}
  */
-export function Note({ text, ...props }) {
-	useEffect(() => {
-		setTimeout(() => {
-			document.querySelector('#notefiction')?.classList.add(styles.active);
-		}, 2000);
-	}, [text]);
-
-	return (
-		<div className={styles.block} {...props} id="notefiction">
-			{text}
-		</div>
+export function Note({ text, isOpen, setIsOpen, ...props }) {
+	return createPortal(
+		<div
+			className={cn(styles.block, {
+				[styles.hide]: !isOpen,
+			})}
+			onClick={() => setIsOpen(false)}
+			{...props}
+		>
+			<span>{text}</span>
+			<Alarm className={cn(styles.icon, styles.bad)} />
+		</div>,
+		portal,
 	);
 }
